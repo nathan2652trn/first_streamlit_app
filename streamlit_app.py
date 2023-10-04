dd30c056e6b81e230c5b7404eaf004639e02b5c3
nathan2652trn <146683716+nathan2652trn@users.noreply.github.com>
2023-10-03 19:10:52 -0700

    Update streamlit_app.py
    Mod 12, adding title to advice

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+# New section to display advice.
+streamlit.header("Fruityvice Fruit Advice!")
+
 # New section to display fruityvice response.
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
