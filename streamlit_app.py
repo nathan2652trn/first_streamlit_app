d4f47106ca1d478e7b3e5d79b6b4c83aace8adb2
nathan2652trn <146683716+nathan2652trn@users.noreply.github.com>
2023-10-03 19:34:10 -0700

    Update streamlit_app.py
    Mod 16, remove json display

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 # New section to display fruityvice response.
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+# streamlit.text(fruityvice_response.json())
 
 # normalize json into table record 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
