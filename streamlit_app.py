24e07203fcf4737618ec8b9ce2d006cc74302a90
nathan2652trn <146683716+nathan2652trn@users.noreply.github.com>
2023-10-04 19:43:14 -0700

    Update streamlit_app.py
    Mod 26, add fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,3 +49,7 @@ my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
 my_data_rows = my_cur.fetchall()
 streamlit.header("Data pull from Snowflake:")
 streamlit.dataframe(my_data_rows)
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
+streamlit.write('The user entered ', fruit_choice)
+
