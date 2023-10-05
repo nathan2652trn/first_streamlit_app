b84cf7de479da30028580e65b5b65eac4d2e61a8
nathan2652trn <146683716+nathan2652trn@users.noreply.github.com>
2023-10-04 21:29:17 -0700

    Update streamlit_app.py
    Mod 49, more functions

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,28 +39,40 @@ try:
   else: 
     back_from_function = get_fruitadvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
-#     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#     streamlit.dataframe(fruityvice_normalized)
 
 except URLError as e:
   streamlit.error()
 
 # Stop streamlit 
-streamlit.stop()
+# streamlit.stop()
 
 # Calls to snowflake 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+# my_cur = my_cnx.cursor()
+# my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+# my_data_row = my_cur.fetchone()
+# streamlit.text("Hello from Snowflake:")
+# streamlit.text(my_data_row)
 
-my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
-my_data_rows = my_cur.fetchall()
-streamlit.header("Data pull from Snowflake:")
-streamlit.dataframe(my_data_rows)
+# my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
+# my_data_rows = my_cur.fetchall()
+# streamlit.header("Data pull from Snowflake:")
+# streamlit.dataframe(my_data_rows)
+
+# Calls to snowflake 
+streamlit.text('The fruit load list contains:') 
+def get_fruit_load_list:
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("select * from fruit_load_list")
+        return my_cur.fetchall()
+
+if streamlit.button('Get Fruit Load List'): 
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
+        
+# Stop streamlit 
+streamlit.stop()
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
