202315d56360bf56a03316918618946ce66a6c1f
nathan2652trn <146683716+nathan2652trn@users.noreply.github.com>
2023-10-04 20:19:53 -0700

    Update streamlit_app.py
    Mod 36, new format

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,30 +22,26 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-# Display the table on the page.
+# Display the table on the page
 streamlit.dataframe(fruits_to_show)
 
-# New section to display advice.
+# New section to display advice
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice
+    streamlit.error('Please select a fruit to get information.'
+  else 
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError.e:
+  streamlit.error()
 
-# New section to display fruityvice response.
-# import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-# streamlit.text(fruityvice_response.json())
-
-# normalize json into table record 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# display table record
-streamlit.dataframe(fruityvice_normalized)
-
-# stop streamlit 
+# Stop streamlit 
 streamlit.stop()
 
-# calls to snowflake 
-# import snowflake.connector
-
+# Calls to snowflake 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
